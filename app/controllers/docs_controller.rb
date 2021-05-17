1a3d1c936369dee24a02ed7fb7a18d8cc02bb369
Talmacel Marian-Silviu <magicace1988@gmail.com>
2021-05-17 09:46:07 +0100

    created form partial

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -7,11 +7,18 @@ class DocsController < ApplicationController
     end
 
     def new
+        @doc = Doc.new
         
     end
 
     def create
-        
+        @doc = Doc.new(doc_params)
+
+        if @doc.save
+            redirect_to @doc
+        else
+            render `new`
+        end
     end
 
     def edit
@@ -33,6 +40,6 @@ class DocsController < ApplicationController
         end
 
         def doc_params
-            
+            params.require(:doc).permit(:title, :content)
         end
 end
